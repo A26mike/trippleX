f1d2b5cbf0f73ecf526f270ac1b011e275f2d25a
Michael Fitzpatrick <a26mike@gmail.com>
2021-04-24 14:13:17 -0400

    working game

--- trippleX.cpp
+++ trippleX.cpp
@@ -1,25 +1,123 @@
+// cl trippleX.cpp /EHsc
 #include <iostream>
 #include <ctime>
+#include <cstdlib>
+
+void PrintIntroduction(int Difficulty)
+{
+    if (Difficulty == 1)
+    {
+        std::cout << R"(
+                                                            
+                                                _=====_                               _=====_
+                                                / _____ \                             / _____ \
+                                            +.-'_____'-.---------------------------.-'_____'-.+
+                                            /   |     |  '.        S O N Y        .'  |  _  |   \
+                                            / ___| /|\ |___ \                     / ___| /_\ |___ \
+                                            / |      |      | ;  __           _   ; | _         _ | ;
+                                            | | <---   ---> | | |__|         |_:> | ||_|       (_)| |
+                                            | |___   |   ___| ;SELECT       START ; |___       ___| ;
+                                            |\    | \|/ |    /  _     ___      _   \    | (X) |    /|
+                                            | \   |_____|  .','" "', |___|  ,'" "', '.  |_____|  .' |
+                                            |  '-.______.-' /       \ANALOG/       \  '-._____.-'   |
+                                            |               |       |------|       |                |
+                                            |              /\       /      \       /\               |
+                                            |             /  '.___.'        '.___.'  \              |
+                                            |            /                            \             |
+                                            \          /                              \           /
+                                            \________/                                \_________/
+                                                               
+
+ .----------------.  .----------------.  .----------------.  .----------------.  .----------------.  .----------------.  .----------------.       .----------------. 
+| .--------------. || .--------------. || .--------------. || .--------------. || .--------------. || .--------------. || .--------------. |     | .--------------. |
+| |  _________   | || |  _______     | || |     _____    | || |   ______     | || |   ______     | || |   _____      | || |  _________   | |     | |  ____  ____  | |
+| | |  _   _  |  | || | |_   __ \    | || |    |_   _|   | || |  |_   __ \   | || |  |_   __ \   | || |  |_   _|     | || | |_   ___  |  | |     | | |_  _||_  _| | |
+| | |_/ | | \_|  | || |   | |__) |   | || |      | |     | || |    | |__) |  | || |    | |__) |  | || |    | |       | || |   | |_  \_|  | |     | |   \ \  / /   | |
+| |     | |      | || |   |  __ /    | || |      | |     | || |    |  ___/   | || |    |  ___/   | || |    | |   _   | || |   |  _|  _   | |     | |    > `' <    | |
+| |    _| |_     | || |  _| |  \ \_  | || |     _| |_    | || |   _| |_      | || |   _| |_      | || |   _| |__/ |  | || |  _| |___/ |  | |     | |  _/ /'`\ \_  | |
+| |   |_____|    | || | |____| |___| | || |    |_____|   | || |  |_____|     | || |  |_____|     | || |  |________|  | || | |_________|  | |     | | |____||____| | |
+| |              | || |              | || |              | || |              | || |              | || |              | || |              | |     | |              | |
+| '--------------' || '--------------' || '--------------' || '--------------' || '--------------' || '--------------' || '--------------' |     | '--------------' |
+ '----------------'  '----------------'  '----------------'  '----------------'  '----------------'  '----------------'  '----------------'       '----------------' 
+        
+        
+        
+        )";
+    }
+
+    std::cout << "\nYou are a secret agent breaking into LEVEL " << Difficulty << " server room...\n";
+    std::cout << "Your SuperHacker 2000 tells you the following information...\n";
+}
+
+
+int GetRandomNum(int start, int end){
+    int results = start + (rand() % end);
+    return results;
+}
+
+bool PlayGame(int Difficulty)
+{
+    //random number 1 - Difficulty
+    const int CodeA = GetRandomNum(1, Difficulty);
+    const int CodeB = GetRandomNum(1 , Difficulty);
+    const int CodeC = GetRandomNum(1 , Difficulty);
+
+    PrintIntroduction(Difficulty);
+    const int CodeProduct = CodeA * CodeB * CodeC;
+    const int CodeSum = CodeA + CodeB + CodeC;
+
+    std::cout << "\n+ There are three numbers in the code\n";
+    std::cout << "+ The codes multiply to give " << CodeProduct << "\n";
+    std::cout << "+ The codes add-up to " << CodeSum << "\n";
+
+    std::cout << "\nEnter the three code numbers followed by x \n";
+
+    int GuessA, GuessB, GuessC;
+    int GuessSum, GuessProduct;
+
+    //get player guess
+    std::cin >> GuessA;
+    std::cin >> GuessB;
+    std::cin >> GuessC;
+
+    GuessSum = GuessA + GuessB + GuessC;
+    GuessProduct = GuessA * GuessB * GuessC;
+
+    if ((GuessSum == CodeSum) && (GuessProduct == CodeProduct))
+    {
+        std::cout << "You won\n\n";
+
+        return true;
+    }
+
+    else
+    {
+        std::cout << "You lost\n\n";
+        return false;
+    }
+}
 
 int main()
-{   
-    int level = 2;
+{
+    int LevelDifficulty = 1;
+    bool EndGame = false;
+    while (!EndGame)
+    {
+        bool bLevelComplete = PlayGame(LevelDifficulty);
 
-    int code1 = 4;
-    int code2 = 3;
-    int code3 = 2;
+        // stop infinate loop if char is entered
+        std::cin.clear();  // clears errors
+        std::cin.ignore(); // Discards the buffer
 
-    int codeProduct = code1 * code2 * code3;
-    int codeSum = code1 + code2 + code3;
-
-    std::cout << "\nYou are a secret agent breaking into LEVEL " << level << " server room..." << std::endl;
-    std::cout << "Your SuperHacker 2000 tells you the following information..." << std::endl;
-
-    std::cout << "\nThere are three numbers in the code"  << std::endl;
-    std::cout << "The codes multiply to give " << codeProduct << std::endl;
-    std::cout << "The codes add-up to " << codeSum << std::endl;
-
-    std::cout << "\nEnter the three code numbers followed by x " << std::endl;
-
+        if (bLevelComplete)
+        {
+            //increase level difficulty
+            ++LevelDifficulty;
+        }
+        else
+        {
+            EndGame = true;
+        }
+    }
     return 0;
 }
